54094b79676481ad43829f037314de6598be79f6
Potitcroissant <maildebibi2@gmail.com>
2025-11-15 18:32:56 +0100

    Fin page miniprojet

--- miniprojet/programmes/miniprojet.sh
+++ miniprojet/programmes/miniprojet.sh
@@ -1,40 +1,38 @@
+#!/usr/bin/env bash
+
 if [ $# -ne 1 ]
 then
 	echo "Ce programme demande un seul argument"
-		exit
+    exit 1
 fi
 
 URLS=$1
-x=1
+lineno=1
 
 OUTPUT="./tableaux/tableau-fr.html"
 
 {
-echo -e "
-<!DOCTYPE html>
-<html lang=\"fr\">
-<head>
-    <meta charset=\"UTF-8\"/>
-    <title>Tableau</title>
-</head>
-<style>
-    table, th, td{
-    border: 1px solid black;
-    border-collapse: collapse;
-    }
-</style>
+echo -e "<!DOCTYPE html>
+        <html lang=\"fr\">
+            <head>
+                <meta charset=\"UTF-8\"/>
+                <link rel=\"stylesheet\" href=\"https://cdn.jsdelivr.net/npm/bulma@1.0.4/css/bulma.min.css\" />
+                <title>Tableau</title>
+            </head>
 
-<body>
-    <h2>Tableau récapitulatif</h2>
-    <table>
-        <tr>
-            <th>N°</th>
-            <th>URL</th>
-            <th>Code</th>
-            <th>Mots</th>
-            <th>Encodage</>
-        </tr>
-"
+            <body>
+                <div class=\"content has-text-inherit\">
+                    <h2 class=\"title is-2 has-background-primary pl-5 py-5\">Tableau récapitulatif</h2>
+                    <table class=\"table is-bordered is-hoverable m-6\">
+                        <thead class=\"has-background-warning\">
+                            <tr>
+                                <th>N°</th>
+                                <th>URL</th>
+                                <th>Code</th>
+                                <th>Mots</th>
+                                <th>Encodage</th>
+                            </tr>
+                        </thead>"
 
 while read -r line;
 do
@@ -42,20 +40,21 @@ do
     encodage=$(curl -is ${line} | grep "charset" | cut -f 3 -d " ")
     mots=$(lynx -dump -nolist ${line} | wc -w)
     
-	echo -e "
-        <tr>
-            <td>${x}</td>
-            <td>${line}</td>
-            <td>${code_HTTP}</td>
-            <td>${mots}</td>
-            <td>${encodage}</td>
-        </tr>
-        "
-	x=$(expr $x + 1)
+	echo -e "<tbody>
+                <tr>
+                    <td>${lineno}</td>
+                    <td>${line}</td>
+                    <td>${code_HTTP}</td>
+                    <td>${mots}</td>
+                    <td>${encodage}</td>
+                </tr>
+            </tbody>"
+	lineno=$(expr $lineno + 1)
 done < $URLS
 
-echo -e "
-    </table>
-</body>
+echo -e "</table>
+        </div>
+    </body>
 </html>"
+
 } > "$OUTPUT"
